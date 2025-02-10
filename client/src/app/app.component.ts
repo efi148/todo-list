cbc4860ecf4cffd60f96fda5261f10ecb2ecfca1
efraim.gelder <119013869+efi98@users.noreply.github.com>
2025-02-10 19:52:45 +0200

    feat(ui): Implement basic to-do list functionality
    Adds input field, add, delete, and toggle functionality.
    Connects to API to persist changes.
    Improves styling with a container and basic element styling.
    Uses FormsModule for two way data binding.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,20 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from "./interfaces";
 import { ApiService } from "./services/api.service";
-import { Observable } from "rxjs";
 import { baseUrl } from "./consts";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { first, firstValueFrom } from "rxjs";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, HttpClientModule],
+    imports: [CommonModule, HttpClientModule, FormsModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     providers: [ApiService]
 })
 export class AppComponent implements OnInit {
-    todos$: Observable<Todo[]> = new Observable<Todo[]>();
+
+    todos: Todo[] = [];
+
 
     constructor(private readonly apiService: ApiService) {
     }
@@ -25,9 +28,33 @@ export class AppComponent implements OnInit {
     }
 
     getTodos(): void {
-        this.todos$ = this.apiService.get(baseUrl + '\\todos') as Observable<Todo[]>;
-        this.apiService.get(baseUrl + '\\todos').subscribe(d => {
-            console.log(d);
+        this.apiService.get(baseUrl + '\\todos').pipe(first()).subscribe((data: any) => {
+            this.todos = data;
+            console.log(data);
         })
+    }
+
+    addTodo(title: string) {
+        if (!title.trim()) return;
+        const todo: Partial<Todo> = {title, description: ''};
+        firstValueFrom(this.apiService.post(baseUrl + '\\todos', todo)).then(() => {
+            this.getTodos();
+        });
+    }
+
+    updateTodo(todo: Todo) {
+        this.apiService.patch(baseUrl + '\\todos\\' + todo.id, {title: todo.title, checked: todo.checked}).pipe(first()).subscribe(() => {
+            this.getTodos();
+        });
+    }
+
+    toggleChecked(todo: Todo) {
+        this.updateTodo(todo);
+    }
+
+    deleteTodo(id: number) {
+        this.apiService.delete(baseUrl + '\\todos\\' + id).pipe(first()).subscribe(() => {
+            this.getTodos();
+        });
     }
 }
