9d6ff585e6ccd2bca394df351f8f7f02aea88a2e
efraim.gelder <119013869+efi98@users.noreply.github.com>
2025-09-01 20:13:47 +0300

    refactor(api): remove unused Inject import from ApiService

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
 import { Observable } from "rxjs";
 
